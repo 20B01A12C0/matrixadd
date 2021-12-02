e8fb66162b0ca2429de896ee37c8fd739b5a0ecf
deepika1603 <94235716+deepika1603@users.noreply.github.com>
2021-12-03 00:10:26 +0530

    Add files via upload

---
+++ Matrixadd.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+class Addmatrix {
+public static void main(String arg[])
+{
+int row , col,i,j;
+Scanner in=new Scanner(System.in);
+System.out.println("enter the number of rows");
+row = in.nextInt();
+System.out.println("enter the number of columns");
+col=in.nextInt();
+int mat1[][]=new int [row][col];
+int mat2[][]=new int [row][col];
+int res[][]=new int [row][col];
+System.out.println("enter the elements of matrix 1");
+int[][] mat=null;
+for(i=0;i<row;i++)
+    {
+	for(j=0;j<col;j++)
+      { 
+		mat[i][j]=in.nextInt();
+         System.out.println();
+      } 
+	}
+System.out.println("entre the elements of matrix 2");
+for(i=0;i<row;i++)
+{for(j=0;j<col;j++)
+{mat[i][j]=in.nextInt();
+System.out.println();
+}}
+for(i=0;i<row;i++)
+{
+for(j=0;j<col;j++)
+{res[i][j]=mat1[i][j]+mat2[i][j];
+}}
+System.out.println("sum of matrices is");
+for(i=0;i<row;i++)
+{for(j=0;j<col;j++)
+{System.out.println(res[i][j]+"/t");
+System.out.println();
+}
+}
+}
+}
